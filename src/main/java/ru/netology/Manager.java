55aa016d0b53d87ccd694144c2d0db2c2dcda091
Alex Kochutov <alex.kochutov@gmail.com>
2022-10-26 01:42:40 +0400

    feat: extend Manager class file

--- src/main/java/ru/netology/Manager.java
+++ src/main/java/ru/netology/Manager.java
@@ -11,14 +11,10 @@ public class Manager {
         repo.save(product);
     }
 
-    public boolean matches(Product product, String text) {
-        return product.getName().contains(text);
-    }
-
     public Product[] searchBy(String text) {
         Product[] result = new Product[0];
         for (Product product : repo.findAll()) {
-            if (matches(product, text)) {
+            if (product.matches(text)) {
                 Product[] tmp = new Product[result.length + 1];
                 for (int i = 0; i < result.length; i++) {
                     tmp[i] = result[i];
